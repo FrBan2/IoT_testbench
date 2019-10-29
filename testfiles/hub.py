fc8bc0383e17cb3db1ae61964efe97ce9a376ded
FrBan2 <45486513+FrBan2@users.noreply.github.com>
2019-10-29 15:40:25 -0400

    Update and rename server_test.py to hub.py

--- testfiles/hub.py
+++ testfiles/hub.py
@@ -6,11 +6,9 @@ import time
 import datetime
 import sqlite3
 
-# The focus of this is to get a server client communication established
-# where the client can send a message to a server and the server log
-# the data in the most basic communication
-# this is so we may troubleshoot the IOT Test bench communication and
-# locate the problem of why its waiting for communication
+# The focus of this is to establish an always listening server/hub
+# This hub can recieve communication from all the test clients, 
+# then store that data in one general database using SQLite
 
 # c.execute('''CREATE TABLE testing (Entry, Data, Client Address, Date Recieved)''')
 
@@ -29,6 +27,9 @@ print("Waiting for a connection ..")
 while True:
     conn = sqlite3.connect(
         '/home/pi/Desktop/iot_test_bench/testfiles/testing.db')
+    c = conn.cursor()
+    sql = 'create table if not exists '+ 'testing' + '(Entry, Data, Client Address, Date Recieved)'
+    c.execute(sql)
     connection, address = s.accept()
     ip_address, port = str(address[0]), str(address[1])
     try:
@@ -40,7 +41,6 @@ while True:
             array[0]), str(array[1]), str(array[2]), str(array[3])
 
         count = count+1
-        c = conn.cursor()
         print(data)
         print("Connection Established with: ", ip_address, "\n Through port: ",
               port, "\n", appliance, "\n", message, "\n ", mac_address, "\n", datetime)
