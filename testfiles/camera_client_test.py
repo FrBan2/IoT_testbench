fd9e609622524acdad9254e96ce509119da07d1d
FrBan2 <45486513+FrBan2@users.noreply.github.com>
2019-10-29 14:26:24 -0400

    Update camera_client_test.py

--- testfiles/camera_client_test.py
+++ testfiles/camera_client_test.py
@@ -1,39 +1,36 @@
-##This is the ref client portion still in testing this
-##this current program has set hours of activity with randomized minutes and seconds
-
 import socket
 import re
 import datetime
-import time 
+import time
 from uuid import getnode as get_mac
 import random
 
+# This is the camera client portion still in testing
+# this current program has set hours of activity with randomized minutes and seconds
 
-
-h=["07","07","08","08","08","08","15","15","16","16","17","17","17","17","18","18"]
+h = ["07", "07", "08", "08", "08", "08", "15", "15",
+     "16", "16", "17", "17", "17", "17", "18", "18"]
 
 for h in h:
 
-    host = '192.168.1.101'
-    port= 8000
+    host = '192.168.1.100'
+    port = 8000
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,5)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 5)
     mac = get_mac
-    mac_addr=(':'.join(re.findall('..', '%012x'% get_mac())))
+    mac_addr = (':'.join(re.findall('..', '%012x' % get_mac())))
 
-    s.connect((host,port))
+    s.connect((host, port))
 
-    min=random.randint(0,60)
-    sec=random.randint(0,60)
-    
-    message=('Camera,In Use,'+str(mac_addr).upper()+','+datetime.datetime.now().strftime("%m-%d-%Y")+'-'+str(h)+'-'+str(min)+'-'+str(sec)+";")
+    min = str(random.randint(0, 60)).zfill(2)
+    sec = str(random.randint(0, 60)).zfill(2)
+
+    message = ('Camera,In Use,'+str(mac_addr).upper()+',' +
+               datetime.datetime.now().strftime("%m-%d-%Y")+'-'+str(h)+'-'+(min)+'-'+(sec)+";")
     print(message)
-  
+
     s.sendall(message.encode("utf-8"))
- 
+
     time.sleep(2)
-   
+
 s.close()
-
-
-
