3ab12e30b1e91a3f4e52ee0413ebac60d28642b7
FrBan2 <45486513+FrBan2@users.noreply.github.com>
2019-10-29 14:22:15 -0400

    Update server_test.py

--- testfiles/server_test.py
+++ testfiles/server_test.py
@@ -1,32 +1,32 @@
-#The focus of this is to get a server client communication established
-#where the client can send a message to a server and the server log
-#the data in the most basic communication
-#this is so we may troubleshoot the IOT Test bench communication and
-#locate the problem of why its waiting for communication
-import multiprocessing 
+# The focus of this is to get a server client communication established
+# where the client can send a message to a server and the server log
+# the data in the most basic communication
+# this is so we may troubleshoot the IOT Test bench communication and
+# locate the problem of why its waiting for communication
+import multiprocessing
 import socket
 import re
 import sys
 import time
 import datetime
 import sqlite3
-#c.execute('''CREATE TABLE testing (Entry, Data, Client Address, Date Recieved)''')
+# c.execute('''CREATE TABLE testing (Entry, Data, Client Address, Date Recieved)''')
 
- 
 host = ''
-port= 8000
+port = 8000
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#bind socket to specified port
+# bind socket to specified port
 s.bind((host, port))
 s.listen(6)
 count = 0
 print("Waiting for a connection ..")
 
-#decode the data
+# decode the data
 while True:
-    conn = sqlite3.connect('/home/pi/Desktop/iot_test_bench/testfiles/testing.db')
+    conn = sqlite3.connect(
+        '/home/pi/Desktop/iot_test_bench/testfiles/testing.db')
     connection, address = s.accept()
     ip_address, port = str(address[0]), str(address[1])
     try:
@@ -34,18 +34,21 @@ while True:
         data = connection.recv(1024)
         datadecode = data.decode('utf-8')
         array = datadecode.split(",")
-        appliance, message, mac_address, datetime= str(array[0]), str(array[1]), str(array[2]), str(array[3])
-        
-        count=count+1
-        c=conn.cursor()
+        appliance, message, mac_address, datetime = str(
+            array[0]), str(array[1]), str(array[2]), str(array[3])
+
+        count = count+1
+        c = conn.cursor()
         print(data)
-        print("Connection Established with: ",ip_address,"\n Through port: ", port,"\n",appliance, "\n",message,"\n ",mac_address,"\n", datetime)
+        print("Connection Established with: ", ip_address, "\n Through port: ",
+              port, "\n", appliance, "\n", message, "\n ", mac_address, "\n", datetime)
         print(' Data '+str(count))
         print("Awaiting new data")
-        c.execute("INSERT INTO testing VALUES (?,?,?,?,?)",(datetime, appliance, message, ip_address, mac_address))
+        c.execute("INSERT INTO testing VALUES (?,?,?,?,?)",
+                  (datetime, appliance, message, ip_address, mac_address))
         conn.commit()
         conn.close()
-    
+
     except socket.error:
         print("Connection Failed...")
         conn.close()
